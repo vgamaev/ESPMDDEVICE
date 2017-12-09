b24ffdeae58d4ee02b5334c29329ce14a5ac6d76
vgamaev <gamaev.va@gmail.com>
2017-12-09 10:27:50 +0300

    9.12.2017
    Sonoff S20 suport

--- ESPMDDEVICE.h
+++ ESPMDDEVICE.h
@@ -3,9 +3,9 @@
 #define WIFI_LED
 
 
-//#define Sonoff_Touch
+#define Sonoff_Touch          //Подходит и для розетки Sonoff S20 примечание ниже
 //#define Sonof_T1_2_button
-#define Sonof_T1_3_button
+//#define Sonof_T1_3_button
 
 #ifdef Sonoff_Touch
   #define MAX_RELAY  1
@@ -21,3 +21,8 @@
   #define MAX_RELAY  3
   #define MAX_BUTTON 3
 #endif
+
+/* При компиляции для выключателей Sonoff выбераем Generic ESP8285 Module
+ *  При компиляции для выключателя Sonoff s20 с флеш памятью PN25F08B выбераем Generic ESP8266 Module 
+ *  flash mode DOUT, flash size 1M(64K SPIFFS)
+ */
