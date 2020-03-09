f9283c2d635c357e80cb9ebea848a211fb0fc666
vgamaev <gamaev.va@gmail.com>
2020-03-09 19:26:08 +0300

    9032020

--- ENERGOMERA.h
+++ ENERGOMERA.h
@@ -4,12 +4,6 @@
 #include "ESPMDDEVICE.H"
 
 #ifdef ENERGOMERA
-/*  #include <Arduino.h>
-  #include <IRremoteESP8266.h>
-  #include <ESP8266WebServer.h>
-  
-  extern ESP8266WebServer server; // веб сервер
-*/
   
 #include <SoftwareSerial.h>
 #include <ESP8266HTTPClient.h>
@@ -19,7 +13,6 @@ extern HTTPClient http; // веб клиент
 
 extern long Previous ; 
 extern int Step ;
-//extern char x [50];
 
 // открываем сессию
 extern byte CmdOpenSesion[];
